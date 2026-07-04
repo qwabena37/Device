c5f5ddcee3cbdf4827411d5e307eed92c0ee8c01
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-07-04 21:27:16 +0000

    modify laptop.js

--- src/data/laptops.js
+++ src/data/laptops.js
@@ -188,5 +188,233 @@ export const laptops = [
     images: [
       "https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=1200"
     ]
-  }
+  },
+  {
+  id: 11,
+  name: "HP EliteBook 840 G3",
+  brand: "HP",
+  processor: "Intel Core i5 6th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '14" HD',
+  graphics: "Intel HD Graphics",
+  battery: "Up to 8 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 2200,
+  discountPrice: 2050,
+  images: [
+    "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=1200"
+  ]
+},
+{
+  id: 12,
+  name: "HP EliteBook 830 G5/G6",
+  brand: "HP",
+  processor: "Intel Core i5 8th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '13.3" FHD',
+  graphics: "Intel UHD Graphics",
+  battery: "Up to 10 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 2700,
+  discountPrice: 2550,
+  images: [
+    "https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?w=1200"
+  ]
+},
+{
+  id: 13,
+  name: "HP EliteBook 830 G6 X360",
+  brand: "HP",
+  processor: "Intel Core i5 8th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '13.3\" Touchscreen',
+  graphics: "Intel UHD Graphics",
+  battery: "Up to 10 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 3200,
+  discountPrice: 3050,
+  images: [
+    "https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?w=1200"
+  ]
+},
+{
+  id: 14,
+  name: "HP EliteBook 830 G7",
+  brand: "HP",
+  processor: "Intel Core i5 10th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '13.3" FHD',
+  graphics: "Intel UHD Graphics",
+  battery: "Up to 11 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 3000,
+  discountPrice: 2800,
+  images: [
+    "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=1200"
+  ]
+},
+{
+  id: 15,
+  name: "HP EliteBook 840 G7",
+  brand: "HP",
+  processor: "Intel Core i5 10th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '14" FHD',
+  graphics: "Intel UHD Graphics",
+  battery: "Up to 11 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 3150,
+  discountPrice: 2950,
+  images: [
+    "https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=1200"
+  ]
+},
+{
+  id: 16,
+  name: "HP EliteBook 1030 G3 X360",
+  brand: "HP",
+  processor: "Intel Core i5 8th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '13.3" Touchscreen',
+  graphics: "Intel UHD Graphics",
+  battery: "Up to 10 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 3600,
+  discountPrice: 3400,
+  images: [
+    "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?w=1200"
+  ]
+},
+{
+  id: 17,
+  name: "MacBook Pro 2017",
+  brand: "Apple",
+  processor: "Intel Core i5",
+  ram: "8GB",
+  storage: "128GB SSD",
+  display: '13.3" Retina',
+  graphics: "Intel Iris Graphics",
+  battery: "Up to 8 Hours",
+  os: "macOS",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 2700,
+  discountPrice: 2500,
+  images: [
+          "/images/Mac17.PNG"
+  ]
+},
+{
+  id: 18,
+  name: "MacBook Pro 2015",
+  brand: "Apple",
+  processor: "Intel Core i7",
+  ram: "16GB",
+  storage: "256GB SSD",
+  display: '15.4" Retina',
+  graphics: "Intel Iris Pro",
+  battery: "Up to 7 Hours",
+  os: "macOS",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 2500,
+  discountPrice: 2300,
+  images: [
+    "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?w=1200"
+  ]
+},
+{
+  id: 19,
+  name: "MacBook Pro 2015",
+  brand: "Apple",
+  processor: "Intel Core i7",
+  ram: "16GB",
+  storage: "512GB SSD",
+  display: '15.4" Retina',
+  graphics: "Intel Iris Pro",
+  battery: "Up to 7 Hours",
+  os: "macOS",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 2700,
+  discountPrice: 2500,
+  images: [
+    "/images/Mac15.PNG"
+  ]
+},
+{
+  id: 20,
+  name: "Dell Latitude 7400 X360 Touch",
+  brand: "Dell",
+  processor: "Intel Core i5 8th Gen",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '14" Touchscreen',
+  graphics: "Intel UHD Graphics",
+  battery: "Up to 10 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 3100,
+  discountPrice: 2900,
+  images: [
+    "https://images.unsplash.com/photo-1541807084-5c52b6b3adef?w=1200"
+  ]
+},
+{
+  id: 21,
+  name: "HP EliteBook 835 G8",
+  brand: "HP",
+  processor: "AMD Ryzen 5 Pro",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '13.3" FHD',
+  graphics: "AMD Radeon Graphics",
+  battery: "Up to 12 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 3000,
+  discountPrice: 2800,
+  images: [
+    "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=1200"
+  ]
+},
+{
+  id: 22,
+  name: "HP EliteBook 745 G6",
+  brand: "HP",
+  processor: "AMD Ryzen 5 Pro",
+  ram: "8GB",
+  storage: "256GB SSD",
+  display: '14" FHD',
+  graphics: "2GB Dedicated Graphics",
+  battery: "Up to 9 Hours",
+  os: "Windows 11 Pro",
+  condition: "Used - Excellent",
+  warranty: "3 Months",
+  price: 2600,
+  discountPrice: 2400,
+  images: [
+    "https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=1200"
+  ]
+}
 ];
